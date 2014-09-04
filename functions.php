e03eae5c13f9b1c69b0b7d9b591782819d8cd78e
Connor Gaughan <connor@clubhousecreative.com>
2014-09-04 11:04:50 -0500

    Image Updates

--- functions.php
+++ functions.php
@@ -13,19 +13,32 @@
 
 	// add google analytics to footer
 	function add_google_analytics() {
-		if (! is_user_logged_in() ){
-			echo "
-				<script>
-	  				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  				})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		if (! is_user_logged_in() ){ ?>
+			<script type="text/javascript">
+				var sc_project=9451687; 
+				var sc_invisible=1; 
+				var sc_security="b69f0ce2"; 
+				var scJsHost = (("https:" == document.location.protocol) ?
+				"https://secure." : "http://www.");
+				document.write("<sc"+"ript type='text/javascript' src='" +
+				scJsHost+
+				"statcounter.com/counter/counter.js'></"+"script>");
+			</script>
+			<noscript>
+				<div class="statcounter">
+					<a title="website statistics" href="http://statcounter.com/" target="_blank"><img class="statcounter" src="http://c.statcounter.com/9451687/0/b69f0ce2/1/" alt="website statistics"></a>
+				</div>
+			</noscript>
+			<script>
+	  			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	  				ga('create', 'UA-46281118-1', 'hikecon.com');
-	  				ga('send', 'pageview');
-				</script>
-			";
-		}
+	  			ga('create', 'UA-46281118-1', 'hikecon.com');
+	  			ga('send', 'pageview');
+			</script>
+		<?php }
 	}
 	add_action('wp_footer', 'add_google_analytics');
 
@@ -56,7 +69,8 @@
 	add_image_size( 'medium', 800, 9999 );
 	add_image_size( 'small', 600, 9999 );
 	add_image_size( 'thumb', 450, 9999 );
-	add_image_size( 'gallery', 550, 550, array( 'center', 'center' ));
+	add_image_size( 'people', 200, 200, array( 'center', 'center' ) );
+	add_image_size( 'gallery', 550, 550, array( 'center', 'center' ) );
 	
 	// Page Excerpts
 	add_action( 'init', 'my_add_excerpts_to_pages' );
